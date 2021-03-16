2eb7997b767cca016124f5c526732da0c0bd6f58
pab6750 <pablo.scarpati99@gmail.com>
2021-03-16 16:02:41 +0000

    slider Finished

--- sliderMap.py
+++ sliderMap.py
@@ -8,22 +8,33 @@ dataset = io.open_file("pleiades-locations-latest.csv")
 #names_data = io.open_file("pleiades-names-latest.csv")
 #places_data = io.open_file("pleiades-places-latest.csv")
 
+#preprocessing
 dataset['timeRange'] = dataset['maxDate'] - dataset['minDate']
 dataset.drop(
     dataset[dataset['timeRange'] > 60000].index, inplace=True)
 dataset.drop(
     dataset[dataset['reprLat'] < 0].index, inplace=True)
-dataset.drop(dataset[~(
-    dataset['featureType'] == "settlement")].index, inplace=True)
+
+
+#slider
+scales = alt.selection_interval(bind='scales')
+minDateSlider = alt.binding_range(min=-750, max=2000, step=10)
+minDateSel = alt.selection_single(name="minDate", fields=['minDate'],
+                                   bind=minDateSlider, init={'minDate': -750})
 
 chart = alt.Chart(dataset).mark_circle().encode(
     x='reprLong:Q',
     y='reprLat:Q',
-    color='featureType',
-    tooltip='timePeriodsKeys'
+    color='timePeriodsKeys',
+    tooltip = 'timePeriodsKeys'
 ).properties(
     width=600,
     height=600
+).add_selection(
+    minDateSel,
+    scales
+).transform_filter(
+    alt.datum.minDate < minDateSel.minDate
 )
 
 chart.show()
