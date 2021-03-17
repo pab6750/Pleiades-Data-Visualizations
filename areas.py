9695920286a00d4640da4d59e9d9014139b5281c
pab6750 <pablo.scarpati99@gmail.com>
2021-03-17 15:00:52 +0000

    Areas.py finally works

--- areas.py
+++ areas.py
@@ -1,3 +1,5 @@
+#excel reference at: https://www.youtube.com/watch?v=QKM7q4fHYOU
+
 import IOReader as io
 import pandas as pd
 import altair as alt
@@ -7,29 +9,39 @@ alt.renderers.enable('altair_viewer')
 alt.data_transformers.disable_max_rows()
 df = pd.read_csv("pleiades-locations-latest.csv")
 
+#the following values are approximations and they may vary depending on the position on the planet.
+distanceBetweenLatitudes = 111000
+distanceBetweenLongitudes = 97000
+
 df['timeRange'] = df['maxDate'] - df['minDate']
-df.drop(df[df['timeRange'] > 60000].index, inplace=True)
-df.drop(df[type(df['bbox']) == "<class 'float'>"].index, inplace=True)
-print(df['bbox'][0])
-# df.loc[:, 'bottomLeftLong'] = df['bbox'].map(lambda x: x)
-df['bottomLeftLong'] = df['bbox'].apply(lambda x: print(type(x)))
-# print(df['bottomLeftLong'])
+df.drop(
+    df[df['timeRange'] > 60000].index, inplace=True)
 
-# df['numericBbox'] = df['bbox'].str.split(", ")
-# df['area'] = df['bottomLeftLong'][0] + df['bottomLeftLong'][1]
+df['area'] = (df['topRightX'] - df['bottomLeftX']) * (df['topRightY'] - df['bottomLeftY'])
 
-# df['area'] = (float(df['bbox'].to_string(index=False, max_rows=1).split(
-#    ", ")[2])       - float(df['bbox'].to_string(index=False, max_rows=1).split(
-#    ", ")[0]))      * (float(df['bbox'].to_string(index=False, max_rows=1).split(
-#    ", ")[3][:-24]) - float(df['bbox'].to_string(index=False, max_rows=1).split(", ")[2]))
+maxArea = df['area'].max()
+minArea = df['area'].min()
 
+originalRange = maxArea - minArea
 
-# print(df['bottomLeftLong'])
-# chart1 = alt.Chart(df).mark_point().encode(
-#    x='timeRange',
-#    y='area',
-#    color='featureType',
-# )
+scaledMax = 3000
+scaledMin = 0
 
+scaledRange = scaledMax - scaledMin
 
-# chart1.show()
+df['scaledArea'] = (((df['area'] - minArea) * scaledRange) / originalRange) + scaledMin
+
+df.drop(df[df['scaledArea'] == 0].index, inplace=True)
+df.drop(df[df.index % 60 != 0].index, inplace=True)
+
+chart = alt.Chart(df).transform_window(
+    index='count()'
+).transform_fold(
+    ['scaledArea', 'maxDate', 'minDate']
+).mark_line().encode(
+    x='key:N',
+    y='value:Q',
+    color='timePeriodsKeys'
+).properties(width=500)
+
+chart.show()
