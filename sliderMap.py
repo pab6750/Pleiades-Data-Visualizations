3ba5da8c6533a78d154fbe845abdabc876960942
pab6750 <pablo.scarpati99@gmail.com>
2021-03-19 16:51:45 +0000

    Finished sliderMap

--- sliderMap.py
+++ sliderMap.py
@@ -8,6 +8,11 @@ dataset = io.open_file("pleiades-locations-latest.csv")
 #names_data = io.open_file("pleiades-names-latest.csv")
 #places_data = io.open_file("pleiades-places-latest.csv")
 
+#location = 'C:\\Users\\pablo\\OneDrive\\Desktop\\uni\\CSC337\\Coursework1\\Europe_NothernAfrica_MiddleAsia_India.json'
+#mapData = alt.Data(url=location, format=alt.DataFormat(property='features',type='json'))
+
+countries = alt.topo_feature(data.world_110m.url, 'countries')
+
 #preprocessing
 dataset['timeRange'] = dataset['maxDate'] - dataset['minDate']
 dataset.drop(
@@ -16,25 +21,35 @@ dataset.drop(
     dataset[dataset['reprLat'] < 0].index, inplace=True)
 
 
+widthValue = 700
+heightValue = 700
+
 #slider
-scales = alt.selection_interval(bind='scales')
 minDateSlider = alt.binding_range(min=-750, max=2000, step=10)
 minDateSel = alt.selection_single(name="minDate", fields=['minDate'],
                                    bind=minDateSlider, init={'minDate': -750})
 
 chart = alt.Chart(dataset).mark_circle().encode(
-    x='reprLong:Q',
-    y='reprLat:Q',
+    longitude='reprLong',
+    latitude='reprLat',
     color='timePeriodsKeys',
     tooltip = 'timePeriodsKeys'
 ).properties(
-    width=600,
-    height=600
+    width=widthValue,
+    height=heightValue,
 ).add_selection(
-    minDateSel,
-    scales
+    minDateSel
 ).transform_filter(
     alt.datum.minDate < minDateSel.minDate
 )
 
-chart.show()
+scaleValue = 350
+translation = [100, 630]
+
+mapData = alt.layer(
+    alt.Chart(countries).mark_geoshape(fill='black'),
+).project(
+    type='equirectangular', scale=scaleValue, translate=translation
+).properties(width=widthValue, height=heightValue).configure_view(stroke=None)
+
+(mapData + chart).show()
