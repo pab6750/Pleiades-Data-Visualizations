31af9aa72994e8350932835691fa625f491ecaab
pab6750 <pablo.scarpati99@gmail.com>
2021-03-24 16:53:01 +0000

    Update SienaMap.py

--- SienaMap.py
+++ SienaMap.py
@@ -1,8 +1,10 @@
 #Siena UID = 62adf57abfdf2645abe652f7caa19880
 #Siena ID = 413293
+#Siena connectsWith = 702,604,147,136,661,000
 
 import pandas as pd
 import altair as alt
+import numpy as np
 from vega_datasets import data
 
 alt.renderers.enable('altair_viewer')
@@ -10,9 +12,10 @@ alt.data_transformers.disable_max_rows()
 df = pd.read_csv("pleiades-places-latest.csv")
 
 df.drop(
-    df[df['id'] < 413293].index, inplace=True)
+     df[df['id'] < 413293].index, inplace=True)
 df.drop(
-    df[df['id'] > 413310 ].index, inplace=True)
+     df[df['id'] > 413310 ].index, inplace=True)
+
 
 chart = alt.Chart(df).mark_circle().encode(
     longitude='reprLong',
@@ -21,3 +24,5 @@ chart = alt.Chart(df).mark_circle().encode(
 )
 
 chart.show()
+
+
